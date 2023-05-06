6297b0bebf6cb96e5a691b0816ab8ef0c5ea71ae
Ricardo Girardi <girardiricardo@outlook.com>
2023-05-06 15:22:23 -0300

    adding register button behaviour

--- src/functional-tests/index.test.js
+++ src/functional-tests/index.test.js
@@ -1,4 +1,6 @@
-import { Selector } from 'testcafe';
+import { Selector, ClientFunction } from 'testcafe';
+
+const getLocation = ClientFunction(() => window.location.href);
 
 fixture `TDD Day Homepage`
   .page('https://tddday.com');
@@ -7,4 +9,13 @@ test('Page should load and display the correct title', async (t) => {
   const actual = Selector('h1').innerText;
   const expected = 'TDD DAY';
   await t.expect(actual).eql(expected);
+});
+
+test('Register button should navigate to registration page', async (t) => {
+  const registerButton = Selector('span').withText('REGISTER NOW');
+  const expected = 'https://zoom.us/webinar/register/WN_rYdjYdXFTPiHCsiWsnq0jA#/registration';
+
+  await t.click(registerButton);
+
+  await t.expect(getLocation()).eql(expected);
 })
